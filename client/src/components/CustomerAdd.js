c94ab2af2190706a8dc37e72b2a69b0fdfe56bd7
Amber-Kim <amber.kje@gmail.com>
2022-09-10 16:49:23 -0400

    Add Material UI Design for Add/Delete

--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -1,5 +1,19 @@
 import React, { Component } from 'react';
 import { post } from 'axios';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+    hidden: {
+        display: 'none'
+    }
+});
+
 
 class CustomerAdd extends Component {
 
@@ -11,7 +25,8 @@ class CustomerAdd extends Component {
       birthday: '',
       gender: '',
       job: '',
-      fileName: ''
+      fileName: '',
+      open: false
     }
   }
 
@@ -41,7 +56,26 @@ class CustomerAdd extends Component {
       birthday: '',
       gender: '',
       job: '',
-      fileName: ''
+      fileName: '',
+      open: false
+    })
+  }
+
+  handleClickOpen = () => {
+    this.setState({
+      open: true
+    })
+  }
+
+  handleClose = () => {
+    this.setState({
+      file: null,
+      userName: '',
+      birthday: '',
+      gender: '',
+      job: '',
+      fileName: '',
+      open: false
     })
   }
 
@@ -64,21 +98,39 @@ class CustomerAdd extends Component {
     return post(url, formData, config)
   }
 
+
   render() {
+    const { classes } = this.props;
     return (
       <div>
-        <form onSubmit={this.handleFormSubmit}>
-          <h1>Add Customer</h1>
-          Profile Image: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange} /><br />
-          Name: <input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange} /><br />
-          DOB: <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange} /><br />
-          Gender: <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange} /> <br />
-          Job: <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange} /> <br />
-          <button type="submit">Add</button>
-        </form>
+        <Button variant="contained" color="primary" onClick={this.handleClickOpen}>
+          Add Customer
+        </Button>
+        <Dialog open={this.state.open} onClose={this.handleClose}>
+          <DialogTitle>Add Customer Info</DialogTitle>
+          <DialogContent>
+            <input className={classes.hidden} accept="image/*" id="raised-button-file" type="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/><br/>
+            <label htmlFor="raised-button-file">
+              <Button variant="contained" color="primary" component="span" name="file">
+                {this.state.fileName === "" ? "Choose the file" : this.state.fileName}
+              </Button>
+            </label><br />
+            <TextField label="Name" name="userName" value={this.state.userName} onChange={this.handleValueChange} /><br />
+            <TextField label="DOB" name="birthday" value={this.state.birthday} onChange={this.handleValueChange} /><br />
+            <TextField label="Gender" name="gender" value={this.state.gender} onChange={this.handleValueChange} /> <br />
+            <TextField label="Job" name="job" value={this.state.job} onChange={this.handleValueChange} /> <br />
+          </DialogContent>
+            <DialogActions>
+              <Button variant="contained" color="primary" onClick={this.handleFormSubmit}>Add</Button>
+              <Button variant="outlined" color="primary" onClick={this.handleClose}>Close</Button>
+            </DialogActions>
+          </Dialog>
+
+
+
       </div>
     );
   }
 }
 
-export default CustomerAdd;
+export default withStyles(styles)(CustomerAdd);
